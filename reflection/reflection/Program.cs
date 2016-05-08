6a80262e4d87bc384dda19ac2eca058973097652
punidev <puni00@yandex.ru>
2016-05-08 18:57:24 +0300

    Sample Virus-Injector
    Fix#3

--- reflection/reflection/Program.cs
+++ reflection/reflection/Program.cs
@@ -47,16 +47,4 @@ namespace reflection
         }
 
     }
-    public class IAssembly
-    {
-        public const string Title = "reflection";
-        public const string Description = "";
-        public const string Configuration = "";
-        public const string Company = "";
-        public const string Product = "reflection";
-        public const string Copyright = "Copyright ©  2016";
-        public const string Trademark = "";
-        public const string Culture = "";
-        public const string Version = "1.0.0.0";
-    }
 }
