24f0895eca4422a62e09454124a07b5edab07015
punidev <puni00@yandex.ru>
2016-08-19 17:37:53 +0300

    UserException
    v2

---
+++ Exc/TestProject/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TestProject
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                int[] mass = new int[10];
+                // ---> 10
+                for (int i = 0; i < 12; i++)
+                // ---> 12
+                {
+                    mass[i] = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                new UserException { Http = ex.GetType().Name }.ShowDialog();
+            }
+        }
+    }
+}
---
+++ Exc/TestProject/UserException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TestProject
+{
+    public partial class UserException : Form
+    {
+        public UserException()
+        {
+            InitializeComponent();
+        }
+
+        public string Http { get; set; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var screenSize = Screen.PrimaryScreen.Bounds.Size;
+            this.Width = screenSize.Width;
+            this.Height = screenSize.Height;
+            Location = new Point(0,0);
+            Text = Http;
+            webBrowser1.Navigate("https://www.google.com.ua/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=" + Http);
+            webBrowser1.ScriptErrorsSuppressed = true;
+        }
+    }
+}
